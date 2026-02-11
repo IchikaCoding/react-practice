794d8aa668c7a24c4e8f7080ea9b211c622cd071
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-02-11 23:40:43 +0900

    feat(App-v2): 商品一覧を絞り込む検索と在庫フィルタを追加

--- src/App-v2.jsx
+++ src/App-v2.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 /**
  * カテゴリを一つずつ表示するコンポーネント
+ *
  * @param {string} category
  * @returns
  */
 function ProductCategoryRow({ category }) {
   return (
     <tr>
+      {/* colSpanは、列を横向きに結合できるもの。今回でいうと、2列を一つのセルとして結合している */}
       <th colSpan="2">{category}</th>
     </tr>
   );
@@ -16,7 +20,7 @@ function ProductCategoryRow({ category }) {
  * @returns
  */
 function ProductRow({ product }) {
-  // 在庫があるなら普通に表示、ないなら赤文字で表示する
+  // 在庫があるなら商品の名前を代入、ないなら表品名が赤文字になるようにspan要素を代入している
   const name = product.stocked ? (
     product.name
   ) : (
@@ -24,6 +28,7 @@ function ProductRow({ product }) {
   );
 
   return (
+    // 1行に2列を表示する
     <tr>
       <td>{name}</td>
       <td>{product.price}</td>
@@ -32,28 +37,44 @@ function ProductRow({ product }) {
 }
 
 /**
- *
- * @param {*} param0
+ * 商品を表示する部分の描画を担当しているコンポーネント
+ * @typedef {Object} Product
+ * @property {string} category
+ * @property {string} price
+ * @property {boolean} stocked
+ * @property {string} name
+ * @param {Product[]} products
  * @returns
  */
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
+  // カテゴリと商品の情報をいれるための配列
   const rows = [];
+  // カテゴリが変わったことを判定するための変数
   let lastCategory = null;
   // forEachは配列の要素一つずつに指定した処理をするメソッド
   products.forEach((product) => {
-    // カテゴリーが変わったことを判定するための条件分岐
+    // 商品の名前を全部小文字にしたものと、検索したい文字として入力された小文字文字列が一致しない場合はリターンする処理
+    // indexOf()は文字列と検索したい文字列が一致しなかったら-1を返すメソッド
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    //! 「ストックがあるものだけ表示するがチェックされているとき」かつ「ストックがないとき」はリターンする
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
     if (product.category !== lastCategory) {
-      //   TODO: rowsはなぜ必要なの？なぜpushしているの？
+      // カテゴリーが変わったことを判定するための条件分岐
       rows.push(
         // カテゴリが変わったら、カテゴリ表示のためのコンポーネントを追加する
         // ! 更新を速く・正しくするためにkeyが必要
+        // propsとしてカテゴリを渡している
         <ProductCategoryRow
           category={product.category}
           key={product.category}
         />,
       );
     }
-    // 商品を表示するためのコンポーネントを追加
+    // 商品を表示するためのコンポーネントを配列に追加
     rows.push(<ProductRow product={product} key={product.name} />);
     lastCategory = product.category;
     console.log("lastCategory", lastCategory);
@@ -73,33 +94,72 @@ function ProductTable({ products }) {
   );
 }
 
-function SearchBar() {
+/**
+ * 入力するフォーム部分を描画するためのコンポーネント
+ * @returns
+ */
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={(e) => {
+          onFilterTextChange(e.target.value);
+        }}
+      />
       <label>
-        <input type="checkbox" /> Only show products in stock
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => {
+            onInStockOnlyChange(e.target.checked);
+          }}
+        />{" "}
+        Only show products in stock
       </label>
     </form>
   );
 }
 
 /**
+ * 検索テキストとチェックボックスの親コンポーネントだからstateはここで管理する
  * @typedef {Object} Product
  * @property {string} category
  * @property {string} price
  * @property {boolean} stocked
  * @property {string} name
- */
-/**
  * @param {Product[]} products
  * @returns
  */
 function FilterableProductTable({ products }) {
+  // 検索テキストのstate変数の初期値
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      {/* チェックされたかどうか、フィルターするときのテキストを受け取るためのprops */}
+      {/* 関数を渡したのは入力値をもらって、その値でstateを更新したいから */}
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      {/* 商品情報、チェックされたかどうか、フィルターするときのテキストを受け取るためのprops */}
+      {/* 更新された値を使えばいいだけだからstateを更新する関数は不要 */}
+      <ProductTable
+        products={products}
+        inStockOnly={inStockOnly}
+        filterText={filterText}
+      />
     </div>
   );
 }
@@ -115,7 +175,8 @@ const PRODUCTS = [
 ];
 
 /**
- *
+ * FilterableProductTableがトップレベルのコンポーネント
+ * propsとして{PRODUCTS}を渡している
  * @returns
  */
 export default function App() {
