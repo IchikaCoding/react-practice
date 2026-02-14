c4aef1717957a0ddce4db44f64ab887df5d93c65
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-02-15 00:08:49 +0900

    add(index.jsx): コードの説明をコメントで追記

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,11 @@ import App from "./App-v2";
 // import App from "./App";
 
 // 最終的な成果物（Appとかスタイルとか全部込みの完成形）をindex.html のid=rootに入れる処理
+// index.htmlのidがroot要素を取得してrootを作成
+// 👉️そのrootのレンダーメソッドを使用して、コンポーネントさんを呼び出す（レンダーする）
 const root = createRoot(document.getElementById("root"));
 root.render(
+  // React は各コンポーネントの関数を 2 回呼び出すことで純粋でない関数が引き起こす間違いに気づきやすくしてくれる
   <StrictMode>
     <App />
   </StrictMode>,
