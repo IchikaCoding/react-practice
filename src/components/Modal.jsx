8efd774e33394ef419db763f245b51095e810904
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-03-03 23:36:39 +0900

    feat: モーダルをBootstrap構造に整えてイベント制御を強化

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,6 +12,8 @@ export default function Modal({ isModalOpen, onConfirm, onCancel }) {
   // イベントリスナー登録したい！これは画面描画以外👉️useEffectの出番！
   // TODO: これって最後に処理される？
   useEffect(() => {
+    // モーダルが閉じていたら早期リターンしてイベント登録しない
+    if (!isModalOpen) return;
     // エスケープキーのハンドラ
     function handleEscapeKeyDown(e) {
       if (e.key === "Escape") {
@@ -32,27 +34,55 @@ export default function Modal({ isModalOpen, onConfirm, onCancel }) {
   }
   // returnされるもの
   return (
-    // モーダル画面をだしているときに暗くなる背景の部分。ここをクリックするとキャンセルされる
-    <div className="modal-overlay" onClick={onCancel}>
-      {/* モーダル画面本体。e.stopPropagation()によって
+    <>
+      <div
+        className="modal d-block"
+        tabIndex={-1}
+        role="dialog"
+        aria-modal="true"
+        onClick={onCancel}
+      >
+        {/* モーダル画面をだしているときに暗くなる背景の部分。ここをクリックするとキャンセルされる */}
+        {/* e.stopPropagation()でダイアログ画面の内側のクリックを外側に伝えないようにしている */}
+        <div
+          className="modal-dialog modal-dialog-centered"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {/* モーダル画面本体。e.stopPropagation()によって
       ダイアログがクリックされても背景までクリックされたことが伝わらない！
       だから、ダイアログを押してもダイアログが閉じることがなくなる！
       */}
-      <div
-        className="modal-dialog"
-        onClick={(e) => e.stopPropagation()}
-        // モーダルってことをアクセシビリティ情報として追加。
-        aria-modal="true"
-        role="dialog"
-        tabIndex={-1}
-        onKeyDown={(e) => {
-          if (e.key === "Escape") onCancel();
-        }}
-      >
-        <p>Are you sure you want to delete?</p>
-        <button onClick={onCancel}>Cancel</button>
-        <button onClick={onConfirm}>OK</button>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Confirm delete</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={onCancel}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Are you sure you want to delete?</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                onClick={onCancel}
+                className="btn btn-secondary"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                onClick={onConfirm}
+                className="btn btn-primary"
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
